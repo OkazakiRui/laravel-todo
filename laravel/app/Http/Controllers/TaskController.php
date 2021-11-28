70c6b4b556b90ac005df1f71d7c7f29f671a3d0c
RuiOkazaki <okazaki200226@gmail.com>
2021-11-28 17:38:50 +0900

    update: task一覧を取得する

--- laravel/app/Http/Controllers/TaskController.php
+++ laravel/app/Http/Controllers/TaskController.php
@@ -15,17 +15,8 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        // return Task::all();
+        return Task::orderByDesc("id")->get();
     }
 
     /**
@@ -46,17 +37,6 @@ class TaskController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Task $task)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Task $task)
     {
         //
     }
