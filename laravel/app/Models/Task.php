5a2a8b722d33005a5462b531d81d9189b0bfdc65
RuiOkazaki <okazaki200226@gmail.com>
2021-11-28 18:51:58 +0900

    update: fillable を作成したのと、is_done の値が数字になっているのを修正した

--- laravel/app/Models/Task.php
+++ laravel/app/Models/Task.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        "title", "is_done"
+    ];
+
+    // is_done の値が 0 になっていた
+    // booleanに変更したい！
+    // カラムの型を指定したい時は $casts 
+    protected $casts = [
+        "is_done" => "bool",
+    ];
 }
