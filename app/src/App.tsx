31885dbac2645f537c01aae24c2be63f77f281d3
RuiOkazaki <okazaki200226@gmail.com>
2021-11-29 20:56:14 +0900

    update: 各種コンポーネントをroutingした

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,33 @@
 import { VFC } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-
-// App.js
-const Home = () => <h2>Home</h2>;
-
-const About = () => <h2>About</h2>;
+import HelpPage from 'components/help/HelpPage';
+import LoginPage from 'components/login/LoginPage';
+import TasksPage from 'components/tasks/TasksPage';
+import './App.css';
 
 const App: VFC = () => (
   <>
+    <header className="global-head">
+      <ul>
+        <li>
+          <Link to="/">Tasks</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/help">Help</Link>
+        </li>
+        <li>
+          <span>Logout</span>
+        </li>
+      </ul>
+    </header>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
+      <Route path="/" element={<TasksPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/help" element={<HelpPage />} />
     </Routes>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-    </ul>
   </>
 );
 
