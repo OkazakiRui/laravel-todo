b5024bd675317862c337a5a0bf687750a93b4b68
RuiOkazaki <okazaki200226@gmail.com>
2021-12-06 16:02:48 +0900

    add: tyoe file を作成した

---
+++ app/src/types/Task.ts
@@ -0,0 +1,10 @@
+export type Task = {
+  id: number;
+  title: string;
+  // eslint-disable-next-line camelcase
+  is_done: boolean;
+  // eslint-disable-next-line camelcase
+  created_at: Date;
+  // eslint-disable-next-line camelcase
+  updated_at: Date;
+};
