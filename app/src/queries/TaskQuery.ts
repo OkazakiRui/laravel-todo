37822eada68ac6d87a9d12b0c68d2bdeaee56102
RuiOkazaki <okazaki200226@gmail.com>
2021-12-06 16:16:24 +0900

    update: Task type を import した

--- app/src/queries/TaskQuery.ts
+++ app/src/queries/TaskQuery.ts
@@ -1,4 +1,5 @@
 import getTasks from 'api/TaskAPI';
+import { Task } from 'types/Task';
 import { useQuery, UseQueryResult } from 'react-query';
 
 const useTasks: () => UseQueryResult<Task[], unknown> = () =>
