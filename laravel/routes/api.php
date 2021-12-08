19634a30cccf3bd4693cde09b6c8114a9c450a14
RuiOkazaki <okazaki200226@gmail.com>
2021-12-08 15:31:04 +0900

    update: routingした

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::patch('tasks/update-done/{id}', [TaskController::class, 'updateDone']);
 Route::resource('tasks', TaskController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
