71d2c16402960673a647e585d9b0a42637bc5e63
RuiOkazaki <okazaki200226@gmail.com>
2021-12-08 14:32:49 +0900

    update: file を分割した

--- app/src/components/tasks/TasksPage.tsx
+++ app/src/components/tasks/TasksPage.tsx
@@ -1,83 +1,11 @@
 import { VFC } from 'react';
-import useTasks from 'queries/TaskQuery';
-import TaskInput from './TaskInput';
+import TaskInput from 'components/tasks/TaskInput';
+import TaskList from 'components/tasks/TaskList';
 
-const TasksPage: VFC = () => {
-  const { data: tasks, status } = useTasks();
-
-  console.log(tasks, status);
-
-  if (status === 'loading') return <div className="margin-top-mid loader" />;
-  if (status === 'error')
-    return (
-      <div className="margin-top-mid align-center">
-        データの読み込みに失敗しました。
-      </div>
-    );
-  if (!tasks || tasks.length <= 0)
-    return (
-      <div className="margin-top-mid align-center">
-        登録されたTODOはありません
-      </div>
-    );
-
-  return (
-    <>
-      <TaskInput />
-      <div className="inner">
-        <ul className="task-list">
-          {tasks.map((task) => (
-            <li key={task.id}>
-              <label className="checkbox-label" htmlFor={String(task.id)}>
-                <input
-                  type="checkbox"
-                  className="checkbox-input"
-                  id={String(task.id)}
-                />
-              </label>
-              <div>
-                <span>{task.title}</span>
-              </div>
-              <button className="btn is-delete" type="button">
-                削除
-              </button>
-            </li>
-          ))}
-
-          <li>
-            <label className="checkbox-label" htmlFor="editTasks">
-              <input type="checkbox" className="checkbox-input" />
-            </label>
-            <form>
-              <input
-                type="text"
-                className="input"
-                defaultValue="編集中のTODO"
-                id="editTasks"
-              />
-            </form>
-            <button className="btn" type="button">
-              更新
-            </button>
-          </li>
-          <li className="done">
-            <label className="checkbox-label" htmlFor="doneTasks">
-              <input
-                type="checkbox"
-                className="checkbox-input"
-                id="doneTasks"
-              />
-            </label>
-            <div>
-              <span>実行したTODO</span>
-            </div>
-            <button className="btn is-delete" type="button">
-              削除
-            </button>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
-};
+const TasksPage: VFC = () => (
+  <>
+    <TaskInput />
+    <TaskList />
+  </>
+);
 export default TasksPage;
