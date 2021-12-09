bda800c758c114b534ce72bd119e9c6b25ac863b
RuiOkazaki <okazaki200226@gmail.com>
2021-12-09 11:35:50 +0900

    update: taskの状態によってcheckbox を変更、更新するようにした

--- app/src/components/tasks/TaskItem.tsx
+++ app/src/components/tasks/TaskItem.tsx
@@ -1,22 +1,32 @@
 import { VFC } from 'react';
 import { Task } from 'types/Task';
+import { useUpdateDoneTask } from 'queries/TaskQuery';
 
 type Props = {
   task: Task;
 };
 
-const TaskItem: VFC<Props> = ({ task }) => (
-  <li key={task.id}>
-    <label className="checkbox-label" htmlFor={String(task.id)}>
-      <input type="checkbox" className="checkbox-input" id={String(task.id)} />
-    </label>
-    <div>
-      <span>{task.title}</span>
-    </div>
-    <button className="btn is-delete" type="button">
-      削除
-    </button>
-  </li>
-);
+const TaskItem: VFC<Props> = ({ task }) => {
+  const updateDoneTask = useUpdateDoneTask();
+
+  return (
+    <li className={task.is_done ? 'done' : ''} key={task.id}>
+      <label className="checkbox-label" htmlFor={String(task.id)}>
+        <input
+          type="checkbox"
+          className="checkbox-input"
+          id={String(task.id)}
+          onClick={() => updateDoneTask.mutate(task)}
+        />
+      </label>
+      <div>
+        <span>{task.title}</span>
+      </div>
+      <button className="btn is-delete" type="button">
+        削除
+      </button>
+    </li>
+  );
+};
 
 export default TaskItem;
